53a45f3a3f5bf420e735f9de4daf51322486d68c
Wdenberg <wdenbergskillshd@hotmail.com>
2024-10-19 21:30:49 -0300

    Seller entity class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,20 @@
 package org.example;
 
 import db.DB.DB;
+import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        System.out.println("Success " + conn);
+
+        Department department = new Department(1, "Developer");
+        Seller seller = new Seller(21,"Wdenberg","wdenberg@gmail.com", new Date(), 5000.00, department );
+
+        System.out.println(seller);
     }
 }
